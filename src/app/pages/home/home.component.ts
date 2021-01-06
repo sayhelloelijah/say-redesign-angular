fb626350188c4f98bc9d09af66c7dbbaed6d571b
sayhelloelijah <say@helloelijah.com>
2021-01-05 22:01:58 -0600

    :construction: work in progress

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,6 +11,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     document.body.classList.value = 'home';
+
+    // const logo = document.body.querySelectorAll('#svg8 path');
+    // logo.forEach(path => {
+    //   console.log(path.getTotalLength());
+    // });
+
+    // const lines = document.body.querySelectorAll('#svg8 rect');
+    // lines.forEach(line => {
+    //   console.log(line.getTotalLength());
+    // });
   }
 
 }
