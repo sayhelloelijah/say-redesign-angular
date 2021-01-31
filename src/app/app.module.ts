11536d76b7aa0227322425f760b72817a4a831d9
sayhelloelijah <say@helloelijah.com>
2021-01-31 11:47:24 -0600

    :art: created partials directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+import { NavComponent } from './partials/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './partials/footer/footer.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { BlogAllComponent } from './pages/blog/blog-all/blog-all.component';
 import { BlogSingleComponent } from './pages/blog/blog-single/blog-single.component';
 import { ProjectAllComponent } from './pages/work/project-all/project-all.component';
 import { ProjectSingleComponent } from './pages/work/project-single/project-single.component';
-import { ResumeComponent } from './resume/resume.component';
+import { ResumeComponent } from './partials/resume/resume.component';
 import { MobileMenuComponent } from './partials/mobile-menu/mobile-menu.component';
 
 @NgModule({
