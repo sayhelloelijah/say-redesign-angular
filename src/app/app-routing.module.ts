a7feaa564b8411f49d3c3502b3e698cf02060b84
sayhelloelijah <say@helloelijah.com>
2021-01-30 19:31:56 -0600

    :iphone: fix mobile styles for home page & mobile navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { ProjectAllComponent } from './pages/work/project-all/project-all.compon
 import { ProjectSingleComponent } from './pages/work/project-single/project-single.component';
 
 const routes: Routes = [
-  {path: '/', component: HomeComponent},
+  {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
   {path: 'blog', component: BlogAllComponent},
   {path: 'blog/blog-single', component: BlogSingleComponent},
